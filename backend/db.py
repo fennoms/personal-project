953eb1b75b04f19065638578009e3a6b6f50a61c
Fennom Schalkwijk <schalkwijkfennom@gmail.com>
2024-07-02 00:55:47 +0200

    🗃️ Create database connection and session

---
+++ backend/db.py
@@ -0,0 +1,22 @@
+"""This file holds the database connection and the logic to
+create all tables in the database."""
+
+import os
+
+from sqlmodel import Session, SQLModel, create_engine
+
+# The database URL is found in the .env file stored under POSTGRES_URL
+DATABASE_URL = os.getenv("POSTGRES_URL")
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+
+def init_db():
+    """Generates database tables."""
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    """Creates a database session."""
+    with Session(engine) as session:
+        yield session
