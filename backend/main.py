5d3aeec85ab30aff2831553b91557e63555c3414
Fennom Schalkwijk <schalkwijkfennom@gmail.com>
2024-07-02 15:37:38 +0200

    🗃️ Create database connection in backend

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,24 @@
 """This file holds the main FastAPI application."""
 
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+
+from db import init_db
+
+
+@asynccontextmanager
+def lifespan() -> None:
+    """Perform startup and shutdown operations."""
+    print("Application startup")
+    init_db()
+    yield
+    print("Application shutdown")
+
 
 app = FastAPI(
     docs_url="/api/docs",
     redoc_url=None,
     openapi_url="/api/openapi.json",
+    lifespan=lifespan,
 )
