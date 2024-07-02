974cde53194dc673c7041f076d6db9427556d7ca
Fennom Schalkwijk <schalkwijkfennom@gmail.com>
2024-07-02 21:45:22 +0200

    ✨ Create login API endpoint

--- backend/routers/auth/routes.py
+++ backend/routers/auth/routes.py
@@ -1,8 +1,9 @@
 """Routes for authentication."""
 
 from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.exc import IntegrityError
-from sqlmodel import Session
+from sqlmodel import Session, select
 
 from db import get_session
 from dependencies import create_access_token
@@ -14,8 +15,10 @@ router = APIRouter(
 )
 
 
-@router.post("/register")
-async def register(user: UserCreateRequest, session: Session = Depends(get_session)):
+@router.post("/register", response_model=UserResponse)
+async def register(
+    user: UserCreateRequest, session: Session = Depends(get_session)
+) -> UserResponse:
     """Register a new user."""
     try:
         new_user = User(
@@ -30,3 +33,21 @@ async def register(user: UserCreateRequest, session: Session = Depends(get_sessi
     token = create_access_token({"id": new_user.id})
 
     return UserResponse(username=new_user.username, id=new_user.id, token=token)
+
+
+@router.post("/login", response_model=UserResponse)
+async def login(
+    user: OAuth2PasswordRequestForm = Depends(), session: Session = Depends(get_session)
+) -> UserResponse:
+    """Login a user."""
+    user = session.exec(select(User).filter(User.username == user.username)).first()
+
+    if user is None:
+        return {"error": "User not found"}
+
+    if user.password != user.password:
+        return {"error": "Invalid password"}
+
+    token = create_access_token({"id": user.id})
+
+    return UserResponse(username=user.username, id=user.id, token=token)
