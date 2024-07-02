507759d41e11c48f061848ec7c09123b283b4f5a
Fennom Schalkwijk <schalkwijkfennom@gmail.com>
2024-07-03 01:22:17 +0200

    🔧 Ensure API_URL is always defined

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,7 +2,7 @@
 
 const nextConfig = {
     env: {
-        API_URL: process.env.NEXT_PUBLIC_API_URL,
+        API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000',
     },
     rewrites: async () => {
         return [
